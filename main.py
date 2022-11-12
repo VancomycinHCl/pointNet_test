2190dd9e4a622de526e1c39f2820a19d8a2b3a84
XuanzhiLiu <sarahclar8956k1@gmail.com>
2022-11-12 19:41:48 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import torch.nn as nn
+import torch
+
+if __name__ == "__main__":
+    print("Hello World!")
+    print(torch.__version__)
+    print("CUDA version:",torch.version.cuda)
---
+++ pointNet_instance/point_net_kernel.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+import torch.functional as F
+
+class T_Net3(nn.Module):
+    def __init__(self):
+        super(T_Net3, self).__init__()
+
+    print("lll")
+
+if __name__ == "main":
+    a = T_Net3()
